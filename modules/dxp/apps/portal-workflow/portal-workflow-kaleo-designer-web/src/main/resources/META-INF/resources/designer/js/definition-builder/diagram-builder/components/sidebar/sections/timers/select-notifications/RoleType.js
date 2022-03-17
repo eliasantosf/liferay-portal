04077e215cc598349e962142cc717ba5a83ec522
Elias Santos <elias.santos@liferay.com>
2022-03-16 23:00:27 -0300

    WIP

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/select-notifications/RoleType.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/select-notifications/RoleType.js
@@ -11,10 +11,16 @@
 
 import React from 'react';
 
-import NotificationsInfo from '../select-notifications/NotificationsInfo';
+import BaseRoleType from '../../shared-components/BaseRoleType';
 
-const ActionTypeNotification = (props) => {
-	return <NotificationsInfo {...props} />;
+const RoleType = (props) => {
+	return (
+		<BaseRoleType
+			buttonName={Liferay.Language.get('new-role-type')}
+			inputLabel={Liferay.Language.get('role-type')}
+			{...props}
+		/>
+	);
 };
 
-export default ActionTypeNotification;
+export default RoleType;
